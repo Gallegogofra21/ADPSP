9a2944f5953c32a8378688ba9cfa764f6f60f1bc
gallego.gofra21 <gallego.gofra21@triana.salesianos.edu>
2022-01-20 12:34:58 +0100

    Correccion

--- EjercicioMockitoSpring/src/test/java/com/salesianostriana/dam/Ejercicio.java
+++ EjercicioMockitoSpring/src/test/java/com/salesianostriana/dam/Ejercicio.java
@@ -7,18 +7,26 @@ import com.salesianostriana.dam.model.Venta;
 import com.salesianostriana.dam.repos.ProductoRepositorio;
 import com.salesianostriana.dam.repos.VentaRepositorio;
 import com.salesianostriana.dam.service.VentaServicio;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 public class Ejercicio {
@@ -29,37 +37,49 @@ public class Ejercicio {
     @MockBean
     VentaRepositorio ventaRepositorio;
 
-    @InjectMocks
     VentaServicio ventaServicio;
+
+    @BeforeEach
+    public void setUp() {
+        ventaServicio = new VentaServicio(productoRepositorio, ventaRepositorio);
+    }
 
     @Test
     void whenNuevaVenta_success() {
 
         Producto p = Producto.builder()
+                .id(1L)
                 .codigoProducto("1")
                 .nombre("Producto")
                 .precio(12.34)
                 .build();
 
         Cliente c = Cliente.builder()
+                .id(1L)
                 .nombre("Rompetechos")
                 .email("rompetechos@prueba.com")
                 .dni("12345678A")
                 .build();
 
+        Map<Long, Integer> carrito = Map.of(1L, 3);
 
-        lenient().when(productoRepositorio.findById(1L)).thenReturn(Optional.of(p));
+        Venta v = Venta.builder()
+                .id(1L)
+                .cliente(c)
+                .lineasDeVenta(List.of(LineaDeVenta.builder()
+                                .producto(p)
+                                .cantidad(3)
+                                .pvp(12)
+                                .build()))
+                .build();
 
-        Map<Long, Integer> carrito = Map.of(1L, 2);
+        when(productoRepositorio.findById(any(Long.class))).thenReturn(Optional.of(p));
 
-        Venta venta = new Venta();
-        venta.setId(2L);
-        venta.setCliente(c);
-        venta.setLineasDeVenta(List.of(new LineaDeVenta(p, 2, 12.34)));
+        when(ventaRepositorio.save(any(Venta.class))).thenReturn(v);
 
-        lenient().when(ventaRepositorio.save(venta)).thenReturn(venta);
+        assertThat(v.equals(ventaServicio.nuevaVenta(carrito, c)));
 
-        assertEquals(venta, ventaServicio.nuevaVenta(carrito, c));
+
     }
 
     @Test
